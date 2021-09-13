eb3c064d6b678324f7512cad721b7dcc6e3bae7c
DavidRapaz <davidrvpersonal97@gmail.com>
2021-09-13 18:27:12 +0100

    added packages for level creation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,37 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Weapon Options")]
     public float damage           = 10f;
     public float range            = 100f;
+
     public int maxMagazineBullets = 10;
+
+    [Header("Score Options")]
+    public int pointsPerShot = 10;
 
     [Header("FPS Camera")]
     public Camera fpsCam;
 
     [Header("UI Elements")]
     public Image crosshairUI;
+
     public GameObject reloadUI;
 
+    public TextMeshProUGUI scoreUI;
+    public TextMeshProUGUI magazineUI;
+
     private int _magazine;
+    private int _score;
 
     void Start()
     {
-        _magazine = maxMagazineBullets;
+        _magazine       = maxMagazineBullets;
+        _score          = 0;
+        magazineUI.text = string.Format("{0} / {1}", _magazine, maxMagazineBullets);
     }
 
     // Update is called once per frame
@@ -37,13 +49,17 @@ public class PlayerController : MonoBehaviour
         RaycastHit hit;
 
         _magazine -= 1;
+        magazineUI.text = string.Format("{0} / {1}", _magazine, maxMagazineBullets);
 
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             EnemyController enemy = hit.transform.GetComponentInParent<EnemyController>();
 
             if (enemy != null)
+            {
                 enemy.TakeDamage(hit.transform.tag, damage);
+                DisplayScore(hit.transform.tag);
+            }
         }
 
         if (_magazine == 0)
@@ -55,9 +71,16 @@ public class PlayerController : MonoBehaviour
 
     void Reload ()
     {
-        _magazine = maxMagazineBullets;
+        _magazine       = maxMagazineBullets;
+        magazineUI.text = string.Format("{0} / {1}", _magazine, maxMagazineBullets);
 
         crosshairUI.gameObject.SetActive(true);
         reloadUI.SetActive(false);
+    }
+
+    void DisplayScore (string tag)
+    {
+        _score += (tag == "Enemy Head" ? pointsPerShot * 2 : pointsPerShot);
+        scoreUI.text = string.Format("{0}", _score);
     }
 }
